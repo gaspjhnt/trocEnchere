3eb4fe83707dae9a26a97f033ff517cd61eb62a1
gjohanet2023 <gjohanet2023@129SE2-CD4MMS3.ad.campus-eni.fr>
2023-03-27 14:29:02 +0200

    [INITIALIZATION] Commit BO (Article, Utilisateur)

--- src/main/java/fr/eni/ecole/trocenchere/bo/Article.java
+++ src/main/java/fr/eni/ecole/trocenchere/bo/Article.java
@@ -1,5 +1,187 @@
 package fr.eni.ecole.trocenchere.bo;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Article {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEnchere;
+	private LocalDate dateFinEnchere;
+	private int prixDepart;
+	private int prixVente; // (Prix qui augmente a chaque enchères)
+	private boolean etatVente; // (Vendu ou non)
+	private Utilisateur utilisateur;
+	private List<Enchere> lstEnchere;
+	private Categorie categorie;
+	private Retrait retrait; // (peux être null pas obliger de le définir dans le constructeur)
+	
+	
+
+	
+	/**
+	 * Constructeur vide pour initialiser un article vide
+	 */
+	public Article() {
+	}
+
+	
+	/**
+	 * Constructeur pour initialiser un article sans adresse de retrait
+	 * INFO : Obligatoire pour créer un article SANS RETRAIT
+	 */
+	public Article(String nomArticle, String description, LocalDate dateDebutEnchere, LocalDate dateFinEnchere,
+			int prixDepart, Categorie categorie) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEnchere = dateDebutEnchere;
+		this.dateFinEnchere = dateFinEnchere;
+		this.prixDepart = prixDepart;
+		this.categorie = categorie;
+	}
+
+	/**
+	 * Constructeur pour initialiser un article avec adresse de retrait
+	 * INFO : Obligatoire pour créer un article avec RETRAIT
+	 */
+	public Article(String nomArticle, String description, LocalDate dateDebutEnchere, LocalDate dateFinEnchere,
+			int prixDepart, Categorie categorie, Retrait retrait) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEnchere = dateDebutEnchere;
+		this.dateFinEnchere = dateFinEnchere;
+		this.prixDepart = prixDepart;
+		this.categorie = categorie;
+		this.retrait = retrait;
+	}
+
+
+	/**
+	 * Constructeur complet
+	 */
+	public Article(int noArticle, String nomArticle, String description, LocalDate dateDebutEnchere,
+			LocalDate dateFinEnchere, int prixDepart, int prixVente, boolean etatVente, Utilisateur utilisateur,
+			List<Enchere> lstEnchere, Categorie categorie, Retrait retrait) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEnchere = dateDebutEnchere;
+		this.dateFinEnchere = dateFinEnchere;
+		this.prixDepart = prixDepart;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.utilisateur = utilisateur;
+		this.lstEnchere = lstEnchere;
+		this.categorie = categorie;
+		this.retrait = retrait;
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateDebutEnchere() {
+		return dateDebutEnchere;
+	}
+
+	public void setDateDebutEnchere(LocalDate dateDebutEnchere) {
+		this.dateDebutEnchere = dateDebutEnchere;
+	}
+
+	public LocalDate getDateFinEnchere() {
+		return dateFinEnchere;
+	}
+
+	public void setDateFinEnchere(LocalDate dateFinEnchere) {
+		this.dateFinEnchere = dateFinEnchere;
+	}
+
+	public int getPrixDepart() {
+		return prixDepart;
+	}
+
+	public void setPrixDepart(int prixDepart) {
+		this.prixDepart = prixDepart;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public boolean isEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(boolean etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+	public List<Enchere> getLstEnchere() {
+		return lstEnchere;
+	}
+
+	public void setLstEnchere(List<Enchere> lstEnchere) {
+		this.lstEnchere = lstEnchere;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	public Retrait getRetrait() {
+		return retrait;
+	}
+
+	public void setRetrait(Retrait retrait) {
+		this.retrait = retrait;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEnchere=" + dateDebutEnchere + ", dateFinEnchere=" + dateFinEnchere + ", prixDepart="
+				+ prixDepart + ", prixVente=" + prixVente + ", etatVente=" + etatVente + ", utilisateur=" + utilisateur
+				+ ", lstEnchere=" + lstEnchere + ", categorie=" + categorie + ", retrait=" + retrait + "]";
+	}
 
 }
--- src/main/java/fr/eni/ecole/trocenchere/bo/Utilisateur.java
+++ src/main/java/fr/eni/ecole/trocenchere/bo/Utilisateur.java
@@ -1,5 +1,215 @@
 package fr.eni.ecole.trocenchere.bo;
 
+import java.util.List;
+
 public class Utilisateur {
+	private int noUtilisateur;
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String telephone;
+	private String rue;
+	private String codePostale;
+	private String Ville;
+	private String motDePasse;
+	private int credit;
+	private boolean administrateur;
+	private List<Enchere> lstEnchere;
+	private List<Article> lstArticle;
+
+	/**
+	 * Constructeur vide pour initialiser un utilisateur
+	 */
+	public Utilisateur() {
+		this.credit = 200;
+	}
+
+	/**
+	 * Constructeur pour initialiser un utilisateur sans listes sans crédit sans
+	 * admin et sans id INFO : obligatoire a la création d'un utilisateur
+	 */
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostale, String ville, String motDePasse) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostale = codePostale;
+		Ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = 200;
+	}
+
+	/**
+	 * Constructeur pour initialiser un utilisateur sans sans ID sans crédit INFO :
+	 * Obligatoire pour créer un admin
+	 */
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostale, String ville, String motDePasse, boolean administrateur, List<Enchere> lstEnchere,
+			List<Article> lstArticle) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostale = codePostale;
+		Ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = 200;
+		this.administrateur = administrateur;
+		this.lstEnchere = lstEnchere;
+		this.lstArticle = lstArticle;
+	}
+
+	/**
+	 * Constructeur entier
+	 */
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostale, String ville, String motDePasse, int credit, boolean administrateur,
+			List<Enchere> lstEnchere, List<Article> lstArticle) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostale = codePostale;
+		Ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+		this.lstEnchere = lstEnchere;
+		this.lstArticle = lstArticle;
+	}
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostale() {
+		return codePostale;
+	}
+
+	public void setCodePostale(String codePostale) {
+		this.codePostale = codePostale;
+	}
+
+	public String getVille() {
+		return Ville;
+	}
+
+	public void setVille(String ville) {
+		Ville = ville;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	public List<Enchere> getLstEnchere() {
+		return lstEnchere;
+	}
+
+	public void setLstEnchere(List<Enchere> lstEnchere) {
+		this.lstEnchere = lstEnchere;
+	}
+
+	public List<Article> getLstArticle() {
+		return lstArticle;
+	}
+
+	public void setLstArticle(List<Article> lstArticle) {
+		this.lstArticle = lstArticle;
+	}
+
+	@Override
+
+	public String toString() {
+		return "Utilisateur [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+				+ prenom + ", email=" + email + ", telephone=" + telephone + ", rue=" + rue + ", codePostale="
+				+ codePostale + ", Ville=" + Ville + ", motDePasse=" + motDePasse + ", credit=" + credit
+				+ ", administrateur=" + administrateur + ", lstEnchere=" + lstEnchere + ", lstArticle=" + lstArticle
+				+ "]";
+	}
 
 }
