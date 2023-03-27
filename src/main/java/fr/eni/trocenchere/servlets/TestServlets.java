1a70b759be60c7c7538cb5fc3457418419316cd3
gjohanet2023 <gjohanet2023@129SE2-CD4MMS3.ad.campus-eni.fr>
2023-03-27 12:12:45 +0200

    premier conflit servlet coco

--- src/main/java/fr/eni/trocenchere/servlets/TestServlets.java
+++ src/main/java/fr/eni/trocenchere/servlets/TestServlets.java
@@ -25,6 +25,9 @@ public class TestServlets extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Premier conflit");
+		
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at non non non: ").append(request.getContextPath());
 	}
