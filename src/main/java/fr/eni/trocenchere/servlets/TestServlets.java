2c84f80f875c7a8f717cb70e6c0e977a2f00b2e5
jdesnoes2023 <jdesnoes2023@129SE2-561MMS3.ad.campus-eni.fr>
2023-03-27 12:13:22 +0200

    Test bug servlet nul de coco

--- src/main/java/fr/eni/trocenchere/servlets/TestServlets.java
+++ src/main/java/fr/eni/trocenchere/servlets/TestServlets.java
@@ -25,7 +25,7 @@ public class TestServlets extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("Trop relou ce truc");
 		response.getWriter().append("Served at non non non: ").append(request.getContextPath());
 	}
 
