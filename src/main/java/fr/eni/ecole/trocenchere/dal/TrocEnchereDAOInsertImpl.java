be46504024b06cbd1eaceddba27233483021f775
jdesnoes2023 <jdesnoes2023@129SE2-561MMS3.ad.campus-eni.fr>
2023-03-28 15:27:10 +0200

    Merge branch 'master' of https://github.com/gaspjhnt/trocEnchere.git

--- src/main/java/fr/eni/ecole/trocenchere/dal/TrocEnchereDAOInsertImpl.java
+++ src/main/java/fr/eni/ecole/trocenchere/dal/TrocEnchereDAOInsertImpl.java
@@ -15,7 +15,7 @@ import fr.eni.ecole.trocenchere.bo.Utilisateur;
 public class TrocEnchereDAOInsertImpl implements TrocEnchereDAOInsert {
 	
 	public static final String INSERT_UTILISATEUR = "INSERT INTO utilisateur(pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-	public static final String INSERT_ARTICLE ="INSERT INTO article(nom,description,date_debut_enchere,date_fin_enchere,prix_depart,prix_vente,etat_vente,Utilisateur_noUtilisateur,Categorie_noCategorie) VALUES(?,?,?,?,?,?,?,?,?)";
+	public static final String INSERT_ARTICLE ="INSERT INTO article(nom,description,date_debut_enchere,date_fin_enchere,prix_depart,etat_vente,Utilisateur_noUtilisateur,Categorie_noCategorie) VALUES(?,?,?,?,?,?,?,?)";
 	public static final String INSERT_ENCHERE ="INSERT INTO enchere(date_enchere,montant_enchere,Utilisateur_noUtilisateur,Article_noArticle) VALUES(?,?,?,?)";
 	public static final String INSERT_CATEGORIE ="INSERT INTO categorie(libelle) VALUES(?)";
 	public static final String INSERT_RETRAIT ="INSERT INTO retrait(rue,code_postal,ville,Article_NoArticle) VALUES(?,?,?,?)";
@@ -63,15 +63,9 @@ public class TrocEnchereDAOInsertImpl implements TrocEnchereDAOInsert {
 			pstmt.setDate(3, Date.valueOf(article.getDateDebutEnchere()));
 			pstmt.setDate(4, Date.valueOf(article.getDateFinEnchere()));
 			pstmt.setInt(5, article.getPrixDepart());
-			pstmt.setInt(6, article.getPrixVente());
-			if(article.isEtatVente()) {
-				pstmt.setInt(7, 1);
-			}
-			else {
-				pstmt.setInt(7, 0);
-			}
-			pstmt.setInt(8, utilisateur.getNoUtilisateur());
-			pstmt.setInt(9, categorie.getNoCategorie());
+			pstmt.setInt(6, 0);
+			pstmt.setInt(7, utilisateur.getNoUtilisateur());
+			pstmt.setInt(8, categorie.getNoCategorie());
 			pstmt.executeUpdate();
 			ResultSet rs = pstmt.getGeneratedKeys();
 			if(rs.next()) {
--- src/main/java/fr/eni/ecole/trocenchere/servlet/ServletTestDAOInsert.java
+++ src/main/java/fr/eni/ecole/trocenchere/servlet/ServletTestDAOInsert.java
@@ -1,6 +1,7 @@
 package fr.eni.ecole.trocenchere.servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.trocenchere.bo.Article;
+import fr.eni.ecole.trocenchere.bo.Categorie;
+import fr.eni.ecole.trocenchere.bo.Enchere;
+import fr.eni.ecole.trocenchere.bo.Retrait;
 import fr.eni.ecole.trocenchere.bo.Utilisateur;
 import fr.eni.ecole.trocenchere.dal.TrocEnchereDAOInsertImpl;
 
@@ -32,13 +37,24 @@ public class ServletTestDAOInsert extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Utilisateur u1 = new Utilisateur("Haste", "Desnoes", "Jérémie", "bloblogmail.com", "0695067182", "Rue du moulin",
 				"35170", "Rennes","Kilokoko30");
+		Retrait r1 = new Retrait("Rue du moulin", "35000", "Rennes");
+		Categorie c1 = new Categorie("Sport");
+		Article a1 = new Article("Velo", "Mon vélo rouge", LocalDate.now(), LocalDate.now(),
+				150,c1);
+		Enchere e1 = new Enchere(LocalDate.now(),300,u1,a1);
+		
 		TrocEnchereDAOInsertImpl dao = new TrocEnchereDAOInsertImpl();
 		try {
-			dao.insertUtilisateur(u1);
+			//dao.insertUtilisateur(u1);
+			//dao.insertCategorie(c1);
+			dao.insertArticle(a1, u1, c1);
+			//dao.insertRetrait(r1, a1);
+			
+			//dao.insertEnchere(e1, u1, a1);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		response.getWriter().append("Coucou ").append(u1.getNoUtilisateur() + "");
+		response.getWriter().append("Coucou ").append(a1.getUtilisateur().getNoUtilisateur() + "");
 	}
 
 	/**
