8bf54e416477f360a5cb1d3f83854f1ae38bf14d
crobin2023 <crobin2023@129SE2-4N8MMS3.ad.campus-eni.fr>
2023-03-28 10:21:45 +0200

    testBO

---
+++ src/main/java/fr/eni/ecole/trocenchere/bo/AppliTestBO.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.trocenchere.bo;
+
+public class AppliTestBO {
+
+	public static void main(String[] args) {
+		Article article = new Article();
+		Categorie categorie = new Categorie();
+		Enchere enchere = new Enchere();
+		Retrait retrait = new Retrait();
+		Utilisateur utilisateur = new Utilisateur();
+	}
+
+}
