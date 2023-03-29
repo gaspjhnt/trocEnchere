4911046f1d8ff446cf8d9391b3936dada2770fd0
gjohanet2023 <gjohanet2023@129SE2-CD4MMS3.ad.campus-eni.fr>
2023-03-29 10:53:05 +0200

    commit jerem

--- src/main/java/fr/eni/ecole/trocenchere/dal/ventearticle/TrocEnchereDAOVendreArticle.java
+++ src/main/java/fr/eni/ecole/trocenchere/dal/ventearticle/TrocEnchereDAOVendreArticle.java
@@ -1,9 +1,19 @@
 package fr.eni.ecole.trocenchere.dal.ventearticle;
 
+import java.util.List;
+
+import fr.eni.ecole.trocenchere.bo.Article;
 import fr.eni.ecole.trocenchere.bo.Categorie;
 
 public interface TrocEnchereDAOVendreArticle {
 
 	public void insertCategorie(Categorie categorie);
+	
+	public void insertArticle(Article article);
+	
+	public List<Categorie> selectAllCategorie();
+	
+	public Categorie selectCategorieById(int id);
+	
 	
 }
