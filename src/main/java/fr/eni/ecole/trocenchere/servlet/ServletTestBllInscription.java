d3cd03b436d986876c8ca7095059141a8bfddc06
jdesnoes2023 <jdesnoes2023@129SE2-561MMS3.ad.campus-eni.fr>
2023-03-29 16:29:19 +0200

    [UPDTATE]Inscription

--- src/main/java/fr/eni/ecole/trocenchere/servlet/ServletTestBllInscription.java
+++ src/main/java/fr/eni/ecole/trocenchere/servlet/ServletTestBllInscription.java
@@ -1,23 +1,28 @@
 package fr.eni.ecole.trocenchere.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.trocenchere.TrocEnchereException;
+import fr.eni.ecole.trocenchere.bll.inscription.InscriptionManagerImpl;
+import fr.eni.ecole.trocenchere.bo.Utilisateur;
+
 /**
- * Servlet implementation class ServletInitialisation
+ * Servlet implementation class ServletTestBllInscription
  */
-@WebServlet("/ServletInitialisation")
-public class ServletInitialisation extends HttpServlet {
+@WebServlet("/ServletTestBllInscription")
+public class ServletTestBllInscription extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletInitialisation() {
+    public ServletTestBllInscription() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +31,15 @@ public class ServletInitialisation extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
+		InscriptionManagerImpl incriptionManager = new InscriptionManagerImpl();
+		try {
+			incriptionManager.ajouter("Haste", "Desnoesafgiagoagjoaigjaigjaoiga", "J", "bloublou@gmail.com", "abougaga", "Rue du moulin",
+					"3517", "Bruz", "zouloulou44");
+		} catch (TrocEnchereException e) {
+			if(e.hasErreurs()) {
+				System.out.println(e.getListeCodesErreur());
+			}
+		}
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
