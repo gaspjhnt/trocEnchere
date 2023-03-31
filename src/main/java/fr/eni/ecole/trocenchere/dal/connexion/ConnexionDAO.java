608ee0eb4d2277b1cf2026732baef26a6f2768be
jdesnoes2023 <jdesnoes2023@129SE2-561MMS3.ad.campus-eni.fr>
2023-03-31 09:44:35 +0200

    [Update TEST] connexion

--- src/main/java/fr/eni/ecole/trocenchere/dal/connexion/ConnexionDAO.java
+++ src/main/java/fr/eni/ecole/trocenchere/dal/connexion/ConnexionDAO.java
@@ -4,7 +4,9 @@ import fr.eni.ecole.trocenchere.TrocEnchereException;
 import fr.eni.ecole.trocenchere.bo.Utilisateur;
 
 public interface ConnexionDAO {
+
+	Utilisateur selectMdpAndPseudo(String pseudoOuMail, String mdp) throws TrocEnchereException;
 	
-	public Utilisateur selectMdpAndPseudo(String pseudo, String mdp) throws TrocEnchereException;
-	public Utilisateur selectMdpAndEmail(String email, String mdp) throws TrocEnchereException;
+	//public Utilisateur selectMdpAndPseudo(String pseudo, String mdp) throws TrocEnchereException;
+	//public Utilisateur selectMdpAndEmail(String email, String mdp) throws TrocEnchereException;
 }
--- src/main/java/fr/eni/ecole/trocenchere/dal/connexion/ConnexionDAOImpl.java
+++ src/main/java/fr/eni/ecole/trocenchere/dal/connexion/ConnexionDAOImpl.java
@@ -10,24 +10,28 @@ import fr.eni.ecole.trocenchere.bo.Utilisateur;
 
 public class ConnexionDAOImpl implements ConnexionDAO {
 	
-	private static final String SELECT_MDP_BY_PSEUDO = "SELECT * FROM utilisateur WHERE pseudo = ? AND mot_de_passe = ?";
-	private static final String SELECT_MDP_BY_EMAIL =  "SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?";
+	//private static final String SELECT_MDP_BY_PSEUDO = "SELECT * FROM utilisateur WHERE pseudo = ? AND mot_de_passe = ?";
+	//private static final String SELECT_MDP_BY_EMAIL =  "SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?";
+	
+	private final String SELECT_STRING_MDP = "SELECT * FROM utilisateur WHERE (pseudo = ? OR email = ?) AND mot_de_passe = ?";
 	
 	@Override
-	public Utilisateur selectMdpAndPseudo(String pseudo, String mdp) throws TrocEnchereException {
+	public Utilisateur selectMdpAndPseudo(String pseudoOuMail, String mdp) throws TrocEnchereException {
 		
 		TrocEnchereException tee = new TrocEnchereException();
 		Utilisateur user = new Utilisateur();
 		
 		try(Connection con = ConnectionProvider.getConnection()){
-			PreparedStatement pstmt = con.prepareStatement(SELECT_MDP_BY_PSEUDO);
-			pstmt.setString(1, pseudo);
-			pstmt.setString(2, mdp);
+			PreparedStatement pstmt = con.prepareStatement(SELECT_STRING_MDP);
+			pstmt.setString(1, pseudoOuMail);
+			pstmt.setString(2, pseudoOuMail);
+			pstmt.setString(3, mdp);
 			ResultSet rs = pstmt.executeQuery();
 			
 			
 		while(rs.next()) {
 			user.setPseudo(rs.getString("pseudo"));
+			user.setEmail(rs.getString("email"));
 			user.setMotDePasse(rs.getString("mot_de_passe"));
 		}
 		}
@@ -39,28 +43,28 @@ public class ConnexionDAOImpl implements ConnexionDAO {
 		return user;
 	}
 	
-	@Override
-	public Utilisateur selectMdpAndEmail(String email, String mdp) throws TrocEnchereException {
-		
-		TrocEnchereException tee = new TrocEnchereException();
-		Utilisateur user = new Utilisateur();
-		
-		try(Connection con = ConnectionProvider.getConnection()){
-			PreparedStatement pstmt = con.prepareStatement(SELECT_MDP_BY_EMAIL);
-			pstmt.setString(1, email);
-			pstmt.setString(2, mdp);
-			ResultSet rs = pstmt.executeQuery();
-			
-		while(rs.next()) {
-			user.setEmail("email");
-			user.setMotDePasse("mot_de_passe");
-		}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			tee.ajouterErreur("Erreur pendant le selectMdpByEmail");
-			throw tee;
-		}
-		return user;
-	}
+//	@Override
+//	public Utilisateur selectMdpAndEmail(String email, String mdp) throws TrocEnchereException {
+//		
+//		TrocEnchereException tee = new TrocEnchereException();
+//		Utilisateur user = new Utilisateur();
+//		
+//		try(Connection con = ConnectionProvider.getConnection()){
+//			PreparedStatement pstmt = con.prepareStatement(SELECT_MDP_BY_EMAIL);
+//			pstmt.setString(1, email);
+//			pstmt.setString(2, mdp);
+//			ResultSet rs = pstmt.executeQuery();
+//			
+//		while(rs.next()) {
+//			user.setEmail("email");
+//			user.setMotDePasse("mot_de_passe");
+//		}
+//		}
+//		catch (SQLException e) {
+//			e.printStackTrace();
+//			tee.ajouterErreur("Erreur pendant le selectMdpByEmail");
+//			throw tee;
+//		}
+//		return user;
+//	}
 }
